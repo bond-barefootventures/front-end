0ae39125ba7e5f6e33fb286bd8cfc02916bf88bb
thucnhu <buitranthucnhu@gmail.com>
2021-09-13 02:05:29 +0700

    finish events and what we do on home

---
+++ src/components/BlogCard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./styling/BlogCard.css"
+
+export default function BlogCard() {
+   return (
+      <div className="blog-card">
+         Nhu
+      </div>
+   )
+}
---
+++ src/components/BusinessCard.js
@@ -0,0 +1,23 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+import "./styling/BusinessCard.css"
+
+export default function BusinessCard(props) {
+   return (
+      <div 
+         className="card" 
+         style={{backgroundColor: props.background, color: props.color}}
+      >
+         <i class={props.icon}></i>
+         <div className="card-info">
+            <h3>{props.title}</h3>
+            <p className="card-description">{props.description}</p>
+            <Link to={props.to} className="card-CTA">
+               <h3>{props.CTA}</h3>
+               <i class="fas fa-chevron-right"></i>
+            </Link>
+         </div>
+      </div>
+   )
+}
