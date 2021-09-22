60c9d1836be16dedf7b2e989820a2880f668e43a
Nhật Phan <scottpm1401@gmail.com>
2021-09-22 12:01:48 +0700

    Organinzed Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react"
-import {Switch, Route} from "react-router-dom"
-
-import {Navbar, Footer} from "./components"
-import {Home, About, Blog, Business, JobSearch, JoinUs} from "./pages"
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+import { Home, About, Blog, Business, JobSearch, JoinUs } from "./pages";
 
 export default function App() {
-	return (
-		<div>
-			<Navbar />
-			<Switch>
-					<Route exact path="/"><Home /></Route>
-					<Route exact path="/about"><About /></Route>
-					<Route exact path="/blog"><Blog /></Route>
-					<Route exact path="/business"><Business /></Route>
-					<Route exact path="/jobs"><JobSearch /></Route>
-					<Route exact path="/join-us"><JoinUs /></Route>
-			</Switch>
-			<Footer />
-		</div>
-	)
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/business" component={Business} />
+        <Route exact path="/jobs" component={JobSearch} />
+        <Route exact path="/join-us" component={JoinUs} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {default as Navbar} from "./Navbar"
-export {default as Footer} from "./Footer"
-export {default as Carousel} from "./Carousel"
-export {default as Banner} from "./Banner"
-export {default as HomeInfo} from "./HomeInfo"
-export {default as BusinessCard} from "./BusinessCard"
-export {default as BlogCard} from "./BlogCard"
+export { default as Navbar } from "./layout/mainLayout/Navbar";
+export { default as Footer } from "./layout/mainLayout/Footer";
+export { default as Carousel } from "./layout/events/Carousel";
+export { default as Banner } from "./layout/Banner/Banner";
+export { default as HomeInfo } from "./layout/pagesInfo/HomeInfo";
+export { default as BusinessCard } from "./layout/Card/BusinessCard";
+export { default as BlogCard } from "./layout/Card/BlogCard";
