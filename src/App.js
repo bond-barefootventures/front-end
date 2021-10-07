e9c675cfb87d6f33d200105e38b598c68d460610
baro <knabao7a7@gmail.com>
2021-10-08 00:50:08 +0700

    adding new pop up model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { Navbar, Footer, SocialIcons } from "./components"
 import { Home, About, Blog, Business, JobSearch, JoinUs, Test } from "./pages"
+import Popup from '../src/components/Popup/index'
 
 export default function App() {
+
+	useEffect(() => {
+		window.onload = function () {
+			setTimeout(function () {
+				return (
+					<Popup />
+				)
+			}, 5000)
+		}
+	})
+
 	return (
 		<Router>
 			<Navbar />
+			<Popup />
 			<SocialIcons />
 			<Switch>
 				<Route exact path="/" component={Home} />
@@ -16,6 +29,7 @@ export default function App() {
 				<Route exact path="/jobs" component={JobSearch} />
 				<Route exact path="/join-us" component={JoinUs} />
 			</Switch>
+
 			<Footer />
 		</Router>
 	)
