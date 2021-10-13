c612ef68920b6086d68bcaa65c7505f1ee6ad3e1
thucnhu <buitranthucnhu@gmail.com>
2021-10-13 22:54:50 +0700

    fixed styling of about-us

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { AboutSlider, AboutContent } from "../../components";
-
-import "./About.css";
+import React from 'react'
+import { AboutSlider, AboutContent } from '../../components'
 
 export default function About() {
-  return (
-    <div className="about">
-      <AboutSlider />
-      <AboutContent />
-    </div>
-  );
+	return (
+		<div className="about">
+			<AboutSlider />
+			<AboutContent />
+		</div>
+	)
 }
